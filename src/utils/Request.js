5eb2fc752020dae0ee54407dcb42a55a5b533fd0
spaezsuarez <spaezsuarez@gmail.com>
2020-12-29 10:54:27 -0500

    Adición de parametros a la ruta para hacer petición get de personajes

--- src/utils/Request.js
+++ src/utils/Request.js
@@ -1,8 +1,10 @@
-import HTTPClient from './HTTPClient';
+import HTTPClient from "./HTTPClient";
 
-export const BASE_URL = 'https://rickandmortyapi.com/api/';
+export const BASE_URL = "https://rickandmortyapi.com/api/";
 const RickApi = new HTTPClient(BASE_URL);
 
-export function getCharacters(page) {
-  return RickApi.get(`character?page=${page}`);
+export function getCharacters({ page = '', name = '', status = '', species = '', gender = '' }) {
+
+  const params = new URLSearchParams({ page, name, status, species, gender });
+  return RickApi.get(`character?${params}`);
 }
