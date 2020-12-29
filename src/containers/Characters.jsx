fa15fb5212a33ee8ef256f3cc9872db5facbc68b
spaezsuarez <spaezsuarez@gmail.com>
2020-12-29 10:53:27 -0500

    Refactor -> uso de hooks y componentes

--- src/containers/Characters.jsx
+++ src/containers/Characters.jsx
@@ -1,18 +1,34 @@
 import React, { useEffect, useState } from "react";
-import CharacterList from "../components/CharacterList/CharacterList";
+//Funciones
 import { getCharacters } from "../utils/Request";
+import useFormControl from '../hooks/useFormControl';
+import useInputControl from '../hooks/useInputControl';
+//Componentes
 import Form from "react-bootstrap/Form";
+import CharacterList from "../components/CharacterList/CharacterList";
+import CharacterFilters from "../components/CharacterFilters/CharacterFilters";
+
 
 const Characters = () => {
+
+  //Variables de estado
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useInputControl(1);
+  
+  //Creación de filtros
+  const [filters, setFilters, onSubmit] = useFormControl({
+    name: "",
+    status: "",
+    species: "",
+    gender: "",
+  });
 
-  // filosofía: Realizar operaciones para SINCRONIZAR el estado
-  useEffect(() => {
+  //Funcion para llamar los datos
+  const getData = (params) => {
     setLoading(true);
-    getCharacters(page)
+    getCharacters(params)
       .then((resData) => {
         setError(null);
         setData(resData);
@@ -23,30 +39,43 @@ const Characters = () => {
         setError(resError);
         setLoading(false);
       });
+  };
+
+  // filosofía: Realizar operaciones para SINCRONIZAR el estado
+  useEffect(() => {
+    getData({ page });
   }, [page]);
 
-  const handlePage = (e) => setPage(parseInt(e.target.value));
+  const handleSubmit = (e) => {
+    getData({ page, ...filters });
+  };
 
   return (
     <>
       <div className="row justify-content-center">
         <div className="col-sm-4">
-          <h3>Ver Personajes</h3>
-          <Form>
+          <center>
+            <h3>Ver Personajes</h3>
+          </center>
+          <Form onSubmit={onSubmit}>
             <Form.Group>
               <Form.Control
                 type="number"
-                placeholder="Enter number"
+                name="page"
+                placeholder="Ingrese el número de la pagina"
                 value={page}
-                onChange={handlePage}
+                onChange={setPage}
               />
-              <Form.Text className="text-muted">
-                Tener en cuenta que maximo se pueden ver 34 paginas.
-              </Form.Text>
             </Form.Group>
           </Form>
         </div>
       </div>
+
+      <CharacterFilters
+        filters={filters}
+        onChange={setFilters}
+        onSubmit={onSubmit(handleSubmit)}
+      />
 
       {loading ? (
         <p>loading...</p>
@@ -54,6 +83,17 @@ const Characters = () => {
         <p>Ha ocurrido un error ({error.message})</p>
       ) : (
         <>
+          <div className="row justify-content-center">
+            <div className="col-sm-4">
+              <Form.Text className="text-muted">
+                <center>
+                  Tener en cuenta que maximo se pueden ver {data.info.pages}{" "}
+                  paginas.
+                </center>
+              </Form.Text>
+            </div>
+          </div>
+
           <CharacterList items={data.results} />
         </>
       )}
