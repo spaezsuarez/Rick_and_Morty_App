d23a4e874260b39208a5d90908fb3f00d7c61e01
spaezsuarez <spaezsuarez@gmail.com>
2021-02-14 10:08:43 -0500

    Creacion del componente generico de ListGrid y Modificación de la card de episodios

--- src/components/Episodes/EpisodeCard/EpisodeCard.jsx
+++ src/components/Episodes/EpisodeCard/EpisodeCard.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { Link } from "react-router-dom";
+import styles from './EpisodeCard.module.scss';
+import imgUno from '../../../../assets/Episode/1.png';
+import imgDos from '../../../../assets/Episode/2.png';
+import imgTres from '../../../../assets/Episode/3.png';
+
+const getImageSeason = (episode) => {
+    let season = episode.substring(0, 3);
+    switch (season) {
+        case "S01":
+            return imgUno;
+        case "S02":
+            return imgDos;
+        case "S03":
+            return imgTres;
+        case "S04":
+            return imgDos;
+        default:
+            return imgDos;
+    }
+}
 
 const EpisodeCard = ({ id, name, air_date, episode }) => {
     return <>
         <Link to={`/episode/${id}`}>
-            <Card bg="info" text="white" className="mb-2">
-                <Card.Img variant="top" src={image} alt={name} title={name} />
-                <Card.Header>
-                    {episode}:{name} 
-                </Card.Header>
-                <Card.Body>
-                    {air_date}
-                </Card.Body>
+            <Card bg="success" text="white" className="mb-2">
+                <Card.Img src={getImageSeason(episode)} alt="Card image" className={styles.i} />
+                <Card.ImgOverlay>
+                    <Card.Title>{episode}:{name}</Card.Title>
+                    <Card.Text>
+                        {air_date}
+                    </Card.Text>
+                </Card.ImgOverlay>
             </Card>
         </Link>
     </>
---
+++ src/components/Shared/ListGrid/ListGrid.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import styles from './ListGrid.module.scss';
+
+function ListGrid({ children }) {
+    
+  return <div className={`${styles.grid} py-3`}>{children}</div>;
+}
+
+export default ListGrid;
