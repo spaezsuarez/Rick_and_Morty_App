c6591d3d5cafe18eaf31388d66dd8e4c7ad4bbc9
spaezsuarez <spaezsuarez@gmail.com>
2021-02-14 10:09:06 -0500

    Robustes en la logica del useFecth

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,31 +1,48 @@
 //Custom hook para pedir datos
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
-const useFetch = (fn,dependencias = []) => {
-    const [state,setState] = useState({
-        data:null,
-        loading:true,
-        error:null
+// Custom Hook para pedir datos
+const useFetch = (fn, deps = []) => {
+  const [isMounted, setIsMounted] = useState(true);
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  const fetchData = () => {
+    setIsMounted(true);
+    setState({
+      data: null,
+      loading: true,
+      error: null,
     });
 
-    useEffect(() => {
-        reFetch();
-    },dependencias);
+    fn()
+      .then(data => {
+        if (isMounted)
+          setState({
+            data,
+            loading: false,
+            error: null,
+          });
+      })
+      .catch(error => {
+        if (isMounted)
+          setState({
+            data: null,
+            loading: false,
+            error,
+          });
+      });
+  };
 
-    const reFetch = () => {
-        fn().then((resData) => {
-            setState({
-                data:resData,loading:false,error:null
-            });
-        }).catch((resError) => {
-            setState({
-                data:null,loading:false,error:resError
-            });
-        });
-    }
+  useEffect(() => {
+    fetchData();
+    return () => setIsMounted(false);
+  }, deps);
 
-    return {...state,reFetch};
- 
+  return { ...state, reFetch: fetchData };
 }
 
 export default useFetch;
